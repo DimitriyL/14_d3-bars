53a51da68a7cf63c1a57d5dad829e2fc00ea6651
DimitriyLeks <dimitriyl@DimitriLekssMBP.fios-router.home>
2018-03-25 19:58:28 -0400

    basics

---
+++ svg.js
@@ -0,0 +1,47 @@
+var pic = document.getElementById("pic");
+var switchButt = document.getElementById("switchYear");
+var year = 2018;
+
+var deptList = ["Agriculture", "Commerce", "Defense", "Education", "Energy", "Health", "Homeland Security", "HUD", "Interior", "Justice", "Labor", "State", "Transportation", "Treasury", "VA"];
+var spending2021 = [123, 10.7, 473, 77.9, 27.3, 1120, 33.5, 31, 13.9, 29.5, 45.6, 20.1, 94.2, 792, 169];
+var spending2018 = [132, 9.56, 491, 65.5, 26.1, 1010, 38.1, 34.7, 13.2, 31.1, 46.6, 23.2, 81.2, 627, 154];
+
+var chart = d3.select(".chart");
+
+var bar = chart.selectAll("div");
+
+var display = function(year){
+
+    if (year == 2018){
+	var barUpdate = bar.data(spending2018);
+    }
+    else{
+	var barUpdate = bar.data(spending2021);
+    }
+    
+    var barEnter = barUpdate.enter().append("div");
+    
+    barEnter.transition().duration(3500).style("width", function(d){
+	return d  + "px";
+    });
+    
+    barEnter.text(function(d){
+	return d;
+    });
+
+}
+
+var switchYear = function(e){
+    bar.innerHTML = "";
+    if(year == 2018){
+	year = 2021;
+    }
+    else{
+	year = 2018;
+    }
+    display(year);
+}
+
+//display(year);
+
+switchButt.addEventListener("click", switchYear);
